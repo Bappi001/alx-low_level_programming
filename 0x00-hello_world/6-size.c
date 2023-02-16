a59ebc4630a4e326e342bf6544a65e93cff8d5c5
Bappi001 <106080035+Bappi001@users.noreply.github.com>
2023-02-16 22:06:50 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - Entry
  * Return: Always (0) (Success)
  */
+
 int main(void)
 {
 	printf("Size of a char: %1u byte(s)\n", sizeof(char));
@@ -11,4 +13,5 @@ int main(void)
 	printf("Size of a long long int: &1u byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %1u byte(s)\n", sizeof(float));
 	return (0);
+	
 }
