a0c88150324c73b206e875d4633d2c91a9bbf337
Antony Kimathi Kamencu <106080035+Bappi001@users.noreply.github.com>
2023-06-19 21:58:33 +0300

    Update 2-strchr.c

--- 0x18-dynamic_libraries/2-strchr.c
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -1,19 +1,11 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- * *_strchr - locates a character in a string,
- * @s: string.
- * @c: character.
- * Return: the pointer to the first occurrence of the character c.
- */
-=======
  * _strchr - Locates a character in a string
  * @s: String
  * @c: Character to search
  * Return: Pointer to the first occurrence of the character c in the string s
  **/
->>>>>>> 2dced31d5121247261a019ef0c251fcee0f88eb7
 
 char *_strchr(char *s, char c)
 {
