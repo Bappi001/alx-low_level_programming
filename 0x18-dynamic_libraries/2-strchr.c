3bd81f76a9f02229702383d44d7348c643c37572
Bappi001 <akimathi019@gmail.com>
2023-06-19 11:52:27 -0700

    Dynamic libraries

--- 0x18-dynamic_libraries/2-strchr.c
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _strchr - locates a character in a string,
+ * *_strchr - locates a character in a string,
  * @s: string.
  * @c: character.
  * Return: the pointer to the first occurrence of the character c.
